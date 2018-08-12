d47c5e81a7d6519832d5a5046d2f979b684926c8
Alexander Webb <alexander.webb@odds-bods.com>
2018-08-12 16:08:31 +0100

    Added Interface and Tutorial bits

--- LD42_unity/Assets/Scripts/Economy.cs
+++ LD42_unity/Assets/Scripts/Economy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Economy : MonoBehaviour {
 	// manages the galaxy economy
 	public float income;				// amount being made per tick
+	public float funds;					// amount stored
+	public float happiness;				// population joy
+	public float devestationUnhappiness = 2f;	// multiplier for how angry destruction makes the people
 	public Starsystem[] allSystems;
 
 	public void Start(){
@@ -19,7 +22,12 @@ public class Economy : MonoBehaviour {
 		foreach(Starsystem sys in allSystems){
 			if (!sys.destroyed){
 				income += sys.gdp * sys.population;
+				happiness += sys.population;
+			} else {
+				happiness -= sys.population * devestationUnhappiness;
 			}
 		}
+
+		funds += income;
 	}
 }
---
+++ LD42_unity/Assets/Scripts/Util/RotateObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public Vector3 rotationAxis;
+	
+	void Update () {
+		transform.Rotate(rotationAxis * Time.deltaTime);
+	}
+}
